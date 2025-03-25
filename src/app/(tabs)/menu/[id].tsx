c4753181a7d46894d0647a8be12af646d6f51bf7
Sameer <akita.sameer1@gmail.com>
2025-03-25 21:51:54 +0900

    add product info page;

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,67 @@
 import { useLocalSearchParams, Stack } from 'expo-router'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native';
+import products from '@/assets/data/products';
+import { defaultPizzaImage } from '@/src/components/ProductListItem';
+
+const sizes = ['S', 'M', 'L', 'XL', ]
 
 export default function ProductDetailsScreen() {
     const { id } = useLocalSearchParams();
 
-    return (
-        <View>
-            <Stack.Screen options={{title: 'Details ' + id}} />
+    const product = products.find((p) => p.id.toString() === id)
 
-            <Text>{id}</Text>
+    if (!product) {
+        return <Text>Product not found</Text>
+    }
+
+    return (
+        <View style={styles.container}>
+            <Stack.Screen options={{title: product.name}} />
+            <Image 
+                source={{ uri: product.image || defaultPizzaImage }}
+                style={styles.image}
+            />
+            <Text>Select size</Text>
+            <View style={styles.sizes}>
+                {sizes.map((size) => (
+                    <View key={size} style={styles.size}>
+                        <Text style={styles.sizeText}>{size}</Text>
+                    </View>
+                ))}
+            </View>
+
+            <Text style={styles.price}>${product.price}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 10,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    },
+    price: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    sizes: {
+        flexDirection: 'row',
+    },
+    size: {
+        backgroundColor: 'gainsboro',
+        borderRadius: '50%',
+        aspectRatio: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10
+    },
+    sizeText: {
+        fontSize: 20,
+        fontWeight: 500
+    }
+});
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '@/src/constants/Colors';
 import { Product } from '../types';
-import { Link } from 'expo-router'
+import { Link } from 'expo-router';
 
 type ProductListItemProps = {
     product: Product;
 }
 
-const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
