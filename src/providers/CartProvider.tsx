ebd67e7cb4b1490deaf155fd0a6b5dd62ce638cc
Sameer <akita.sameer1@gmail.com>
2025-03-26 23:08:22 +0900

    shopping cart

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -5,11 +5,13 @@ import { randomUUID } from "expo-crypto";
 type CartType = {
     items: CartItem[];
     addItem: (product: Product, size: CartItem['size']) => void;
+    updateQuantity: (itemId: string, amount: -1 | 1 ) => void;
 }
 
 export const CartContext = createContext<CartType>({
     items: [],
     addItem: () => {},
+    updateQuantity: () => {},
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -17,6 +19,14 @@ const CartProvider = ({ children }: PropsWithChildren) => {
 
     const addItem = (product: Product, size: CartItem['size']) => {
         // todo: if already in cart, increment quantity
+        const existingItem = items.find(
+            (item) => item.product === product && item.size === size
+        );
+
+        if (existingItem) {
+            updateQuantity(existingItem.id, 1)
+            return
+        }
 
         const newCartItem: CartItem = {
             id: randomUUID(),
@@ -29,9 +39,18 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         setItems([newCartItem, ...items]);
     }
 
+    const  updateQuantity = (itemId: string, amount: -1 | 1 ) => {
+        const updatedItems = items.map((item) => 
+            item.id !== itemId ? item : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
+
+        setItems(updatedItems)
+    };
+
     return (
         <CartContext.Provider 
-            value={{ items, addItem }}
+            value={{ items, addItem, updateQuantity }}
         >
             {children}
         </CartContext.Provider>
