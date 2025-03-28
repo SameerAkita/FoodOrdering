9257f9918379d67ad1907b51a9057e3c9c5c8d16
Sameer <akita.sameer1@gmail.com>
2025-03-28 17:44:36 +0900

    admin side

---
+++ src/app/(admin)/menu/_layout.tsx
@@ -0,0 +1,67 @@
+import { FontAwesome } from '@expo/vector-icons'
+import { Stack, Link } from 'expo-router'
+import { Pressable } from 'react-native'
+import Colors from '@/src/constants/Colors'
+
+export default function MenuStack() {
+    return (
+        <Stack
+            screenOptions={{
+                // headerRight: () => (
+                //     <Link href="/cart" asChild>
+                //         <Pressable>
+                //             {({ pressed }) => (
+                //                 <FontAwesome
+                //                     name="shopping-cart"
+                //                     size={25}
+                //                     color={Colors.light.tint}
+                //                     style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
+                //                 />
+                //             )}
+                //         </Pressable>
+                //     </Link>
+                // )
+            }}
+        >
+            <Stack.Screen 
+                name="index"
+                options={{
+                    title: "Menu",
+                    headerRight: () => (
+                        <Link href="/" asChild>
+                            <Pressable>
+                                {({ pressed }) => (
+                                    <FontAwesome 
+                                        name="plus-square-o"
+                                        size={25}
+                                        color={Colors.light.tint}
+                                        style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                    )
+                }}
+            />
+            <Stack.Screen 
+                name="[id]"
+                options={{
+                    headerRight: () => (
+                        <Link href="/" asChild>
+                            <Pressable>
+                                {({ pressed }) => (
+                                    <FontAwesome 
+                                        name="pencil"
+                                        size={25}
+                                        color={Colors.light.tint}
+                                        style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                    )
+                }}
+            />
+        </Stack>
+    )
+}
