a419e3eaa14cc0bced6c8d667e2816b8666c2948
Sameer <akita.sameer1@gmail.com>
2025-04-10 16:17:02 +0900

    order list admin

--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -1,8 +1,19 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, ActivityIndicator } from 'react-native'
 import orders from '@/assets/data/orders'
 import OrderListItem from '@/src/components/OrderListItem'
+import { useAdminOrderList } from '@/src/api/orders'
 
 export default function Orders() {
+    const { data: orders, isLoading, error } = useAdminOrderList({ archived: true })
+
+    if (isLoading) {
+        return <ActivityIndicator />
+    }
+
+    if (error) {
+        return <Text>Failed to fetch</Text>
+    }
+
     return (
         <FlatList 
             data={orders}
--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -1,8 +1,19 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, ActivityIndicator } from 'react-native'
 import orders from '@/assets/data/orders'
 import OrderListItem from '@/src/components/OrderListItem'
+import { useMyOrderList } from '@/src/api/orders'
 
 export default function Orders() {
+    const { data: orders, isLoading, error } = useMyOrderList()
+
+    if (isLoading) {
+        return <ActivityIndicator />
+    }
+
+    if (error) {
+        return <Text>Failed to fetch</Text>
+    }
+
     return (
         <FlatList 
             data={orders}
